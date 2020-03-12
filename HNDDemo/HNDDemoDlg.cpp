c91e1809ec97a6101d6cf8f5ac0b5206e92ad05a
tracy <runner0502@gmail.com>
2020-03-12 11:06:23 +0800

    新建海能达模块项目

---
+++ HNDDemo/HNDDemoDlg.cpp
@@ -0,0 +1,97 @@
+
+// HNDDemoDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "HNDDemo.h"
+#include "HNDDemoDlg.h"
+#include "afxdialogex.h"
+#include "tinyxml2.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CHNDDemoDlg 对话框
+
+
+
+CHNDDemoDlg::CHNDDemoDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_HNDDEMO_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CHNDDemoDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CHNDDemoDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CHNDDemoDlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CHNDDemoDlg 消息处理程序
+
+BOOL CHNDDemoDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CHNDDemoDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CHNDDemoDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CHNDDemoDlg::OnBnClickedOk()
+{
+	
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
